53319edd19d8abab51590a13f779e05411cf3e58
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-11-22 00:35:22 -0300

    Acesso a banco de dados test

--- src/main/java/com/swproject/dscommerce/controllers/ProductController.java
+++ src/main/java/com/swproject/dscommerce/controllers/ProductController.java
@@ -1,15 +1,27 @@
 package com.swproject.dscommerce.controllers;
 
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.swproject.dscommerce.entities.Product;
+import com.swproject.dscommerce.repositories.ProductRepository;
+
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
-  
+
+  private final ProductRepository productRepository;
+
+  public ProductController(ProductRepository productRepository) {
+    this.productRepository = productRepository;
+  }
+
   @GetMapping
   public String test() {
-    return "Hello World!";
+    Optional<Product> result = productRepository.findById(1L);
+    return result.get().getName();
   }
 }
