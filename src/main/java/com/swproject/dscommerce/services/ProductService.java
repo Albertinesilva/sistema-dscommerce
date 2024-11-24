cf33d5c8be1752465bd4bcbf3166261e22ea1854
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-11-24 20:35:48 -0300

    GeET Product paged

--- src/main/java/com/swproject/dscommerce/services/ProductService.java
+++ src/main/java/com/swproject/dscommerce/services/ProductService.java
@@ -1,6 +1,8 @@
 package com.swproject.dscommerce.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,5 +20,11 @@ public class ProductService {
   public ProductDTO findById(Long id) {
     Product product = productRepository.findById(id).get();
     return new ProductDTO(product);
+  }
+
+  @Transactional(readOnly = true)
+  public Page<ProductDTO> findAll(Pageable pageable) {
+    Page<Product> products = productRepository.findAll(pageable);
+    return products.map(x -> new ProductDTO(x));
   }
 }
